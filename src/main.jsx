3bb2eeed74df5e68c1cbfe283c598713a6aa30f6
khaimayme-gif <khaimay.me@gmail.com>
2026-08-24 09:31:25 +0700

    New Files Uploaded

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DailyOS from './DailyOS.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DailyOS />
+  </React.StrictMode>
+);
